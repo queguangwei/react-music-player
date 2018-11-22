52d9a744a38217ec17e3c971a1ae32251ac5eb8a
queguangwei <queguangwei@ddy98.com>
2018-11-22 09:07:58 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	entry: './app/index.js',
+	output: {
+		path: './dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+				loader: "style!css"
+			},
+			{
+				test: /\.less/,
+				loader: 'style-loader!css-loader!less-loader'
+			}
+		]
+	}
+}
